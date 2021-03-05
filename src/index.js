30be884ff0629b7943007e3d0771e4a3da76c5c8
lnabua <nabua.lorraine@gmail.com>
2021-03-05 00:32:02 -0800

    commit tree acc12b2af56f5b2ecfba40b388b9291b624fc417

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 const init = () => {
+    const inputForm = document.querySelector('form');
+
+    inputForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const input = document.querySelector('input#searchByID');
   
+        console.log(input.value);
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(resp => resp.json())
+        .then(data => {
+            const title = document.querySelector('section#movieDetails h4');
+            const summary = document.querySelector('section#movieDetails p');
+            
+            title.innerText = data.title;
+            summary.innerText = data.summary;
+        });
+    });
 }
 
 document.addEventListener('DOMContentLoaded', init);
+
+/*
+- Add event listeners to capture form data and override a form's default behavior
+- Fetch data based on what the user types into that form
+- Display that data on the page
+*/
